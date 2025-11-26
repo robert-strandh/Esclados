1e66e5415fbb839d61054f861d80c577f7eb39a3
Robert Strandh <robert.strandh@gmail.com>
2025-11-26 10:42:39 +0100

    Remove unused function FULLY-UNLISTED.

--- utils.lisp
+++ utils.lisp
@@ -5,11 +5,6 @@
       (funcall fn obj)
       obj))
 
-(defun fully-unlisted (obj &optional (fn #'first))
-  (if (listp obj)
-      (fully-unlisted (funcall fn obj))
-      obj))
-
 (defun listed (obj)
   (if (listp obj)
       obj
