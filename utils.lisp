2a304ed7678cac2f5cd4c254774885914e3afc10
Robert Strandh <robert.strandh@gmail.com>
2025-11-27 08:25:59 +0100

    Extract the documentation string from BUILD-MENU.

--- utils.lisp
+++ utils.lisp
@@ -17,32 +17,10 @@
       list))
 
 (defun build-menu (command-tables &rest commands)
-  "Create a command table inheriting commands from
-`command-tables', which must be a list of command table
-designators. The created command table will have a menu
-consisting of `commands', elements of which must be one of:
-
-  * A named command accessible in one of `command-tables'. This may
-    either be a command name, or a cons of a command name and
-    arguments. The command will appear directly in the menu.
-
-  * A list of the symbol `:menu' and something that will evaluate
-    to a command table designator. This will create a submenu
-    showing the name and menu of the designated command table.
-
-  * A list of the symbol `:submenu', a string, and a &rest list
-    of the same form as `commands'. This is equivalent to `:menu'
-    with a call to `build-menu' with `command-tables' and
-    the specified list as arguments.
-
-  * A symbol `:divider', which will present a horizontal divider
-    line.
-
- An error of type`command-table-error' will be signalled if a
-command cannot be found in any of the provided command tables."
   (labels ((get-command-name (command)
              (or (loop for table in command-tables
-                       for name = (command-line-name-for-command command table :errorp nil)
+                       for name = (command-line-name-for-command
+                                   command table :errorp nil)
                        when name return name)
                  (error 'command-table-error
                   :format-string "Command ~A not found in any provided command table"
@@ -64,6 +42,36 @@ command cannot be found in any of the provided command tables."
     (make-command-table nil
      :inherit-from command-tables
      :menu (mapcar #'make-menu-entry commands))))
+
+(setf (documentation 'build-menu 'function)
+      (format nil "Create a command table inheriting commands from~@
+                   `command-tables', which must be a list of command~@
+                    table designators. The created command table will~@
+                    have a menu consisting of `commands', elements of~@
+                    which must be one of:~@
+                    ~@
+                    * A named command accessible in one of `command-tables'.~@
+                      This may either be a command name, or a cons of a~@
+                      command name and arguments. The command will appear~@
+                      directly in the menu.~@
+                    ~@
+                    * A list of the symbol `:menu' and something that will~@
+                      evaluate to a command table designator. This will~@
+                      create a submenu showing the name and menu of the~@
+                      designated command table.~@
+                    ~@
+                    * A list of the symbol `:submenu', a string, and a~@
+                      &rest list of the same form as `commands'. This~@
+                      is equivalent to `:menu' with a call to `build-menu'~@
+                      with `command-tables' and the specified list as~@
+                      arguments.~@
+                    ~@
+                    * A symbol `:divider', which will present a horizontal~@
+                      divider line.~@
+                    ~@
+                    An error of type`command-table-error' will be signalled~@
+                    if a command cannot be found in any of the provided~@
+                    command tables."))
 
 (defmacro define-menu-table (name (&rest command-tables) &body commands)
   "Define a command table with a menu named `name' and containing
