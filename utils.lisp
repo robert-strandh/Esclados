a5fd00fbcfb11bfbe6b45a6de04f62e122e86662
Robert Strandh <robert.strandh@gmail.com>
2025-11-28 15:03:16 +0100

    Extract documentation string for DEFINE-MENU-TABLE.

--- utils.lisp
+++ utils.lisp
@@ -74,17 +74,19 @@
                     command tables."))
 
 (defmacro define-menu-table (name (&rest command-tables) &body commands)
-  "Define a command table with a menu named `name' and containing
-`commands'. `Command-tables' must be a list of command table
-designators containing the named commands that will be included
-in the menu. `Commands' must have the same format as the
-`commands' argument to `build-menu'. If `name' already names a
-command table, the old definition will be destroyed."
   `(make-command-table ',name
-    :inherit-from (list (build-menu ',command-tables
-                                    ,@commands))
+    :inherit-from (list (build-menu ',command-tables ,@commands))
     :inherit-menu t
     :errorp nil))
+
+(setf (documentation 'define-menu-table 'function)
+      (format nil "Define a command table with a menu named `name'~@
+                   and containing `commands'. `Command-tables' must~@
+                   be a list of command table designators containing~@
+                   the named commands that will be included in the menu.~@
+                   `Commands' must have the same format as the `commands'~@
+                    argument to `build-menu'. If `name' already names a~@
+                    command table, the old definition will be destroyed."))
 
 (defclass name-mixin ()
   ((%name :accessor name
