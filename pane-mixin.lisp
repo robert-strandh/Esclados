08841efd487b277316751c150ccc6dfae806cd7e
Robert Strandh <robert.strandh@gmail.com>
2025-12-06 07:30:19 +0100

    Provide DEFGENERIC forms for slot accessor PREVIOUS-COMMAND.

--- pane-mixin.lisp
+++ pane-mixin.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:esclados)
 
+(defgeneric previous-command (pane-mixin))
+
+(defgeneric (setf previous-command) (command pane-mixin))
+
 (defclass pane-mixin ()
   (;; allows a certain number of commands to have some minimal memory
    (%previous-command
