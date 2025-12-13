6a78bee55e2f266df58cf101a033b1fd0cd7cb56
Robert Strandh <robert.strandh@gmail.com>
2025-12-13 14:08:49 +0100

    Improve code layout.

--- frame-mixin.lisp
+++ frame-mixin.lisp
@@ -28,19 +28,22 @@
 (defmethod minibuffer ((clim:application-frame esclados-frame-mixin))
   (clim:frame-standard-input clim:application-frame))
 
-(defmethod clim:redisplay-frame-panes :around ((frame esclados-frame-mixin) &key force-p)
+(defmethod clim:redisplay-frame-panes :around
+    ((frame esclados-frame-mixin) &key force-p)
   (declare (ignore force-p))
   (when (null (remaining-keys frame))
     (setf (executingp frame) nil)
     (call-next-method)))
 
-(defmethod clim:execute-frame-command :after ((frame esclados-frame-mixin) command)
+(defmethod clim:execute-frame-command :after
+    ((frame esclados-frame-mixin) command)
   ;; FIXME: I'm not sure that we want to do this for commands sent
   ;; from other threads; we almost certainly don't want to do it twice
   ;; in such cases...
   (setf (previous-command (present-window frame)) command))
 
-(defmethod clim:execute-frame-command :around ((frame esclados-frame-mixin) command)
+(defmethod clim:execute-frame-command :around
+    ((frame esclados-frame-mixin) command)
   (declare (ignore command))
   (call-next-method)
   (when (eq frame clim:*application-frame*)
