f36ee7faac07845f913948c545d7db66eba64640
Robert Strandh <robert.strandh@gmail.com>
2025-12-16 13:22:18 +0100

    No longer USE package ESCLADOS.

--- packages.lisp
+++ packages.lisp
@@ -66,7 +66,7 @@
            #:read-only-p))
 
 (defpackage :esclados-io
-  (:use :clim-lisp :esclados)
+  (:use :clim-lisp)
   (:local-nicknames (#:utils #:esclados-utils)
                     (#:buf #:esclados-buffer))
   (:export #:frame-find-file #:find-file
