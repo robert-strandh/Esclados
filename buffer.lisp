2b60e03ed308d00087478ee3f11dd088c68d71bb
Robert Strandh <robert.strandh@gmail.com>
2025-12-20 13:48:34 +0100

    Extract documentation strings.

--- buffer.lisp
+++ buffer.lisp
@@ -1,29 +1,41 @@
 (cl:in-package #:esclados-buffer)
 
-(defgeneric frame-make-buffer-from-stream (application-frame stream)
-  (:documentation "Create a fresh buffer by reading the external
-representation from STREAM"))
+(defgeneric frame-make-buffer-from-stream (application-frame stream))
+
+(setf (documentation 'frame-make-buffer-from-stream 'function)
+      (format nil "Create a fresh buffer by reading the external~@
+                   representation from STREAM"))
 
 (defun make-buffer-from-stream (stream)
-  "Create a fresh buffer by reading the external representation
-from STREAM"
   (frame-make-buffer-from-stream clim:*application-frame* stream))
 
-(defgeneric frame-make-new-buffer (application-frame &key &allow-other-keys)
-  (:documentation "Create a empty buffer for the application frame."))
+(setf (documentation 'make-buffer-from-stream 'function)
+      (format nil "Create a fresh buffer by reading the external~@
+                   representation from STREAM"))
+
+(defgeneric frame-make-new-buffer (application-frame &key &allow-other-keys))
+
+(setf (documentation 'frame-make-new-buffer 'function)
+      (format nil "Create a empty buffer for the application frame."))
 
 (defun make-new-buffer (&rest args &key &allow-other-keys)
-  "Create a empty buffer for the current frame."
   (apply #'frame-make-new-buffer clim:*application-frame* args))
 
-(defgeneric frame-save-buffer-to-stream (application-frame buffer stream)
-  (:documentation "Save the entire BUFFER to STREAM in the appropriate
-external representation"))
+(setf (documentation 'make-new-buffer 'function)
+      (format nil "Create a empty buffer for the current frame."))
+
+(defgeneric frame-save-buffer-to-stream (application-frame buffer stream))
+
+(setf (documentation 'frame-save-buffer-to-stream 'function)
+      (format nil "Save the entire BUFFER to STREAM in the appropriate~@
+                   external representation"))
 
 (defun save-buffer-to-stream (buffer stream)
-  "Save the entire BUFFER to STREAM in the appropriate external
-representation"
   (frame-save-buffer-to-stream clim:*application-frame* buffer stream))
+
+(setf (documentation 'save-buffer-to-stream 'function)
+      (format nil "Save the entire BUFFER to STREAM in the~@
+                   appropriate external representation"))
 
 (defclass esclados-buffer-mixin (utils:name-mixin)
   ((%filepath :initform nil :accessor filepath)
