4dfeea94679e33a361806f822b6b113891959ce3
Robert Strandh <robert.strandh@gmail.com>
2025-12-21 09:18:42 +0100

    Move definition of package ESCLADOS-BUFFER to file in new system.

---
+++ Buffer/packages.lisp
@@ -0,0 +1,11 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:esclados-buffer
+  (:use :clim-lisp)
+  (:local-nicknames (#:utils #:esclados-utils))
+  (:export #:frame-make-buffer-from-stream #:make-buffer-from-stream
+           #:frame-save-buffer-to-stream #:save-buffer-to-stream
+           #:filepath #:name #:needs-saving #:file-write-time #:file-saved-p
+           #:esclados-buffer-mixin
+           #:frame-make-new-buffer #:make-new-buffer
+           #:read-only-p))
--- packages.lisp
+++ packages.lisp
@@ -51,16 +51,6 @@
            #:com-start-macro #:com-end-macro
            #:com-call-last-macro))
 
-(defpackage #:esclados-buffer
-  (:use :clim-lisp)
-  (:local-nicknames (#:utils #:esclados-utils))
-  (:export #:frame-make-buffer-from-stream #:make-buffer-from-stream
-           #:frame-save-buffer-to-stream #:save-buffer-to-stream
-           #:filepath #:name #:needs-saving #:file-write-time #:file-saved-p
-           #:esclados-buffer-mixin
-           #:frame-make-new-buffer #:make-new-buffer
-           #:read-only-p))
-
 (defpackage #:esclados-io
   (:use :clim-lisp)
   (:local-nicknames (#:utils #:esclados-utils)
