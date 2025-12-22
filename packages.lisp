72815bfcd3705bafcaabf47c516f50a27ad65dd7
Robert Strandh <robert.strandh@gmail.com>
2025-12-22 07:13:20 +0100

    Move definition of package ESCLADOS-IO to file in new system.

--- packages.lisp
+++ packages.lisp
@@ -50,21 +50,3 @@
            #:keyboard-macro-table #:keyboard-macro-menu-table
            #:com-start-macro #:com-end-macro
            #:com-call-last-macro))
-
-(defpackage #:esclados-io
-  (:use :clim-lisp)
-  (:local-nicknames (#:utils #:esclados-utils)
-                    (#:buf #:esclados-buffer)
-                    (#:mini #:esclados-minibuffer))
-  (:export #:frame-find-file #:find-file
-           #:frame-find-file-read-only #:find-file-read-only
-           #:frame-set-visited-file-name #:set-visited-filename
-           #:check-buffer-writability
-           #:frame-save-buffer #:save-buffer
-           #:frame-write-buffer #:write-buffer
-           #:buffer-writing-error #:buffer #:filepath
-           #:filepath-is-directory
-           #:io-table #:esclados-io-menu-table
-           #:com-find-file #:com-find-file-read-only
-           #:com-read-only #:com-set-visited-file-name
-           #:com-save-buffer #:com-write-buffer))
