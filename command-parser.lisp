695b3453b67ffc408d5f521897ae3e178ba03aaa
Robert Strandh <robert.strandh@gmail.com>
2025-12-22 15:21:23 +0100

    Extract MAYBE-CLEAR-INPUT to a separate function.

--- command-parser.lisp
+++ command-parser.lisp
@@ -38,40 +38,40 @@
                  unless (eq key :gesture)
                  collect key and collect (eval val)))))
 
+(defun maybe-clear-input (stream)
+  (let ((gesture (clim:read-gesture :stream stream :peek-p t :timeout 0)))
+    (when (and gesture (or (clim:delimiter-gesture-p gesture)
+                           (clim:activation-gesture-p gesture)))
+      (clim:read-gesture :stream stream))))
+
 (defun command-parser (clim:command-table stream)
   (let ((clim:command-name nil))
-    (flet ((maybe-clear-input ()
-             (let ((gesture (clim:read-gesture :stream stream 
-                                               :peek-p t :timeout 0)))
-               (when (and gesture (or (clim:delimiter-gesture-p gesture)
-                                      (clim:activation-gesture-p gesture)))
-                 (clim:read-gesture :stream stream)))))
-      (clim:with-delimiter-gestures
-          (clim:*command-name-delimiters* :override t)
-        ;; While reading the command name we want use the history of
-        ;; the (accept 'command ...) that's calling this function.
-        ;;
-        ;; FIXME: does this :history nil actually achieve the above?
-        (setq clim:command-name
-              (clim:accept
-               `(clim:command-name :command-table ,clim:command-table)
-               :stream (clim:encapsulating-stream-stream stream)
-               :prompt *extended-command-prompt*
-               :prompt-mode :raw :history nil))
-        (maybe-clear-input))
-      (clim:with-delimiter-gestures
-          (clim:*command-argument-delimiters* :override t)
-        ;; FIXME, except we can't: use of CLIM-INTERNALS.
-        (let* ((info (gethash clim:command-name climi::*command-parser-table*))
-               (required-args (climi::required-args info))
-               (keyword-args (climi::keyword-args info)))
-          (declare (ignore keyword-args))
-          (let (result)
-            ;; only required args for now.
-            (dolist (arg required-args (cons clim:command-name (nreverse result)))
-              (destructuring-bind (name ptype &rest args) arg
-                (push (parse-one-arg stream name ptype args) result)
-                (maybe-clear-input)))))))))
+    (clim:with-delimiter-gestures
+        (clim:*command-name-delimiters* :override t)
+      ;; While reading the command name we want use the history of
+      ;; the (accept 'command ...) that's calling this function.
+      ;;
+      ;; FIXME: does this :history nil actually achieve the above?
+      (setq clim:command-name
+            (clim:accept
+             `(clim:command-name :command-table ,clim:command-table)
+             :stream (clim:encapsulating-stream-stream stream)
+             :prompt *extended-command-prompt*
+             :prompt-mode :raw :history nil))
+      (maybe-clear-input stream))
+    (clim:with-delimiter-gestures
+        (clim:*command-argument-delimiters* :override t)
+      ;; FIXME, except we can't: use of CLIM-INTERNALS.
+      (let* ((info (gethash clim:command-name climi::*command-parser-table*))
+             (required-args (climi::required-args info))
+             (keyword-args (climi::keyword-args info)))
+        (declare (ignore keyword-args))
+        (let (result)
+          ;; only required args for now.
+          (dolist (arg required-args (cons clim:command-name (nreverse result)))
+            (destructuring-bind (name ptype &rest args) arg
+              (push (parse-one-arg stream name ptype args) result)
+              (maybe-clear-input stream))))))))
 
 (defun esclados-partial-command-parser
     (clim:command-table stream clim:command position
@@ -79,38 +79,32 @@
   (declare (ignore clim:command-table position))
   (let ((clim:command-name (car clim:command))
 	(command-args (cdr clim:command)))
-    (flet ((maybe-clear-input ()
-             (let ((gesture (clim:read-gesture :stream stream 
-                                               :peek-p t :timeout 0)))
-               (when (and gesture (or (clim:delimiter-gesture-p gesture)
-                                      (clim:activation-gesture-p gesture)))
-                 (clim:read-gesture :stream stream)))))
-      (clim:with-delimiter-gestures
-          (clim:*command-argument-delimiters* :override t)
-        ;; FIXME, except we can't: use of CLIM-INTERNALS.
-        (let ((info (gethash clim:command-name climi::*command-parser-table*)))
-          (if (null info)
-              ;; `command' is not a real command! Well, we can still
-              ;; replace numeric argument markers.
-              (clim:substitute-numeric-argument-marker clim:command numeric-argument)
-              (let ((required-args (climi::required-args info))
-                    (keyword-args (climi::keyword-args info)))
-                ;; keyword arguments not yet supported
-                (declare (ignore keyword-args))
-                (let (result arg-parsed)
-                  ;; only required args for now.
-                  (do* ((required-args required-args (cdr required-args))
-                        (arg (car required-args) (car required-args))
-                        (command-args command-args (cdr command-args))
-                        (command-arg (car command-args) (car command-args)))
-                       ((null required-args) (cons clim:command-name (nreverse result)))
-                    (destructuring-bind (name ptype &rest args) arg
-                      (push (cond ((eq command-arg clim:*unsupplied-argument-marker*)
-                                   (setf arg-parsed t)
-                                   (parse-one-arg stream name ptype args))
-                                  ((eq command-arg clim:*numeric-argument-marker*)
-                                   (or numeric-argument (getf args :default)))
-                                  (t (eval command-arg)))
-                            result)
-                      (when arg-parsed
-                        (maybe-clear-input))))))))))))
+    (clim:with-delimiter-gestures
+        (clim:*command-argument-delimiters* :override t)
+      ;; FIXME, except we can't: use of CLIM-INTERNALS.
+      (let ((info (gethash clim:command-name climi::*command-parser-table*)))
+        (if (null info)
+            ;; `command' is not a real command! Well, we can still
+            ;; replace numeric argument markers.
+            (clim:substitute-numeric-argument-marker clim:command numeric-argument)
+            (let ((required-args (climi::required-args info))
+                  (keyword-args (climi::keyword-args info)))
+              ;; keyword arguments not yet supported
+              (declare (ignore keyword-args))
+              (let (result arg-parsed)
+                ;; only required args for now.
+                (do* ((required-args required-args (cdr required-args))
+                      (arg (car required-args) (car required-args))
+                      (command-args command-args (cdr command-args))
+                      (command-arg (car command-args) (car command-args)))
+                     ((null required-args) (cons clim:command-name (nreverse result)))
+                  (destructuring-bind (name ptype &rest args) arg
+                    (push (cond ((eq command-arg clim:*unsupplied-argument-marker*)
+                                 (setf arg-parsed t)
+                                 (parse-one-arg stream name ptype args))
+                                ((eq command-arg clim:*numeric-argument-marker*)
+                                 (or numeric-argument (getf args :default)))
+                                (t (eval command-arg)))
+                          result)
+                    (when arg-parsed
+                      (maybe-clear-input stream)))))))))))
