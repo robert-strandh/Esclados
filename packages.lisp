c2433ecd76af06874f47b13d934fbec72e2d4044
Robert Strandh <robert.strandh@gmail.com>
2025-12-28 06:32:27 +0100

    No longer export symbols that are now in different modules.

--- packages.lisp
+++ packages.lisp
@@ -7,16 +7,9 @@
                     (#:cmd #:esclados-command-processor)
                     (#:tbl #:esclados-command-table-manipulation)
                     (#:frame #:esclados-frame))
-  (:export #:*minibuffer* #:minibuffer #:minibuffer-pane #:display-message
-           #:with-minibuffer-stream
-           #:info-pane #:master-pane
-           #:frame-mixin #:recordingp #:executingp
-           #:*abort-gestures* #:*current-gesture* #:*command-processor*
-           #:unbound-gesture-sequence #:gestures
-           #:command-processor #:instant-macro-execution-mixin
+  (:export #:command-processor #:instant-macro-execution-mixin
            #:asynchronous-command-processor #:command-loop-command-processor
            #:dead-key-merging-command-processor
-           #:overriding-handler #:directly-processing-p #:process-gesture #:process-gestures-or-command
            #:command-for-unbound-gestures
            #:*extended-command-prompt*
            #:define-top-level #:top-level #:simple-command-loop
@@ -27,20 +20,9 @@
            #:partial-command-parser
            #:command-table
 
-           #:gesture-matches-gesture-name-p #:meta-digit
-           #:proper-gesture-p
-           #:universal-argument #:meta-minus
-
            ;; General commands
            #:global-table
            #:com-quit #:com-extended-command
-
-           ;; Help commands
-           #:help-table #:help-menu-table
-           #:com-describe-key-briefly #:com-where-is
-           #:com-describe-bindings
-           #:com-describe-key #:com-describe-command
-           #:com-apropos-command
 
            ;; Keyboard macro commands
            #:keyboard-macro-table #:keyboard-macro-menu-table
