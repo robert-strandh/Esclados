61aa319ba1050139f344ca04582c7cfdb0d35b6f
Robert Strandh <robert.strandh@gmail.com>
2025-12-31 06:10:21 +0100

    Change package prefix to KEY (ESCLADOS-STANDARD-KEY-BINDINGS).

--- Example/commands-and-key-bindings.lisp
+++ Example/commands-and-key-bindings.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:esclados-example)
 
 (clim:define-command-table global-example-table
-  :inherit-from (esclados:global-table esclados:keyboard-macro-table))
+  :inherit-from (key:global-table esclados:keyboard-macro-table))
 
 (clim:define-command
     (com-insert-character
