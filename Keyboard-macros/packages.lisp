770527dd27a852644339cf2ce9401836c4ab5d15
Robert Strandh <robert.strandh@gmail.com>
2026-01-01 09:05:13 +0100

    Introduce local nickname for ESCLADOS-UTILS.

--- Keyboard-macros/packages.lisp
+++ Keyboard-macros/packages.lisp
@@ -3,5 +3,6 @@
 (defpackage #:esclados-keyboard-macros
   (:use #:common-lisp)
   (:local-nicknames (#:tbl #:esclados-command-table-manipulation)
-                    (#:cmd #:esclados-command-processor))
+                    (#:cmd #:esclados-command-processor)
+                    (#:utils #:esclados-utils))
   (:export #:keyboard-macro-table))
