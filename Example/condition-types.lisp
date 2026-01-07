c6f3873d2f938dcac3112694436d53c390fbcea9
Robert Strandh <robert.strandh@gmail.com>
2026-01-07 14:12:24 +0100

    New condition type END-OF-BUFFER.

--- Example/condition-types.lisp
+++ Example/condition-types.lisp
@@ -9,3 +9,13 @@
                       the element before the first one in the buffer,~@
                       whereas the cursor is already at the beginning of~@
                       the buffer"))))
+
+(define-condition end-of-buffer ()
+  ()
+  (:report (lambda (condition stream)
+             (declare (ignore condition))
+             (format stream
+                     "An attempt was made to move forward, or to delete~@
+                      the element after the last one in the buffer,~@
+                      whereas the cursor is already at the end of the~@
+                      buffer"))))
